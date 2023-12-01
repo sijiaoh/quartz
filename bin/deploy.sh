2096abf3e06857a248907cbf09187eea4c923338
sijiaoh <sijiaoh@outlook.com>
2023-12-01 11:39:07 +0900

    Refactor deploy.sh.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,13 +7,12 @@ npm run build
 touch public/.nojekyll
 echo 'sijiaoh.com' > public/CNAME
 
-if [ ! -d sijiaoh.github.io ]; then
-  git clone -b gh-pages git@github.com:sijiaoh/sijiaoh.github.io.git
-  cd sijiaoh.github.io
-  git config --local user.email "sijiaoh@outlook.com"
-  git config --local user.name "sijiaoh"
-  cd -
-fi
+rm -rf sijiaoh.github.io
+git clone -b gh-pages --depth 1 git@github.com:sijiaoh/sijiaoh.github.io.git
+cd sijiaoh.github.io
+git config --local user.email "sijiaoh@outlook.com"
+git config --local user.name "sijiaoh"
+cd -
 
 rm -rf sijiaoh.github.io/*
 cp -r public/* sijiaoh.github.io
