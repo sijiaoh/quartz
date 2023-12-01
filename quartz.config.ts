765495eb427a3438bc07af81862cdeb0f75c45bc
sijiaoh <sijiaoh@outlook.com>
2023-12-02 07:59:48 +0900

    Add google analytics.

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-QYXPCTGH1X",
     },
     baseUrl: "sijiaoh.com",
     ignorePatterns: ["private", "templates", ".obsidian", "!(public_media)**/!(*.md)", "!(*.md)"],
